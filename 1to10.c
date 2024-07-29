15d644a728c13b18876da7984abeb40de0d41ca7
Chinu234 <chirayulodha2004@gmail.com>
2024-07-29 20:54:32 +0530

    second message

--- 1to10.c
+++ 1to10.c
@@ -2,7 +2,7 @@
 #include<conio.h>
 void main(){
 	int a=1,num;
-	printf("Enter a number for that you want to print the table");
+	printf("Enter  a number for that you want to print the table");
 	scanf("%d", &num);
 	while(a<=10){
 		printf("%d\n",a*num);
