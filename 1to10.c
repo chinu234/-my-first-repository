f47111b7d7255696656c6d2b019b7a230a0e8040
Chinu234 <chirayulodha2004@gmail.com>
2024-07-29 20:49:36 +0530

    my first commit

---
+++ 1to10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int a=1,num;
+	printf("Enter a number for that you want to print the table");
+	scanf("%d", &num);
+	while(a<=10){
+		printf("%d\n",a*num);
+		a++;
+	}
+}
---
+++ charSwitchEx.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	char ch;
+	printf("Enter a charcter : ");
+	scanf("%c",&ch);
+	switch(ch){
+		case 'a':
+			printf("charcter is vowel and you have entered %c",ch);
+		break;
+		case 'e':
+			printf("character is vowel and you have entered %c",ch);
+		break;
+		case 'i':
+			printf("character is vowel and you have entered %c",ch);
+		break;
+		case 'o':
+			printf("character is vowel and you have entered %c",ch);
+		break;
+		case 'u':
+			printf("character is vowel and you have entered %c",ch);
+		break;
+		    
+		
+		default: printf("non vowel and somthing");
+	}
+}
